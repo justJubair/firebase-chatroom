df60fe142854022f0622017ad5c6ce688358c43d
Jubair <jubair.ahmed2838@gmail.com>
2023-10-29 21:50:25 +0600

    Add: google sign in implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import auth from "./firebase/firebase.config";
+import { useEffect } from "react";
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({});
+  //  sign in with google
+  const googleProvider = new GoogleAuthProvider();
+  const handleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  // sign out
+  const handleSignOut = () => {
+    signOut(auth)
+      .then()
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // setup a observer for user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      return () => {
+        unsubscribe();
+      };
+    });
+  }, []);
+
+
+  // ChatRoom component
+  const ChatRoom = () => {
+    return(
+      <div>
+         <p> HELLO I Am App </p>
+      </div>
+    )}
+ 
+    //Sign in component
+    const SignIn = () => {
+      return(
+        <>
+          
+            <button className="sign-in" onClick={handleSignIn}>Sign in</button>
+        </>
+      )}
+   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <header>
+          <h1>Chity-Chat</h1>
+          {
+            user &&  <button className="sign-out" onClick={handleSignOut}>Sign out</button>
+          }
+        </header>
+        
+
+       <section>
+        {user ? <ChatRoom/> : <SignIn/>}
+       </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
